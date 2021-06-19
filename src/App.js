f619a5b2d93431b3ba80f23dd74c1174a05f54ad
lavrov-webdev <lavrov.webdev@gmail.com>
2021-06-19 12:15:11 +0300

    Check

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { styled } from 'styled-components';
 
+const Wrapper = styled.main`
+  
+`
+
 function App() {
   return (
     <div className="App">
